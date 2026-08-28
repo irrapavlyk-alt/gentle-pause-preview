c7de19ef8da309273c52c5e9b8c954523f8ed848
irrapavlyk-alt <irra.pavlyk@gmail.com>
2026-08-28 19:17:58 +0300

    Add files via upload
    Gentle Pause PWA beta v6 candidate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gentle-pause-pwa-diagnostic-v1";
+const CACHE = "gentle-pause-pwa-beta-v6-candidate";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png", "./icon-1024.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
